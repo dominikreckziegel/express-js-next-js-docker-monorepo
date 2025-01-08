f5e3c7d3001ac15d4498c6e0b3a1057cb99d22c2
dominik.reckziegel <dominik.reckziegel@me.com>
2025-01-08 15:40:51 +0100

    08.01.2025 - 0.0.5 - Setup Frontend and Index.js

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="description" content="Animal Network - Connecting shelters, veterinarians, and helpers" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body className="bg-gray-100 text-gray-900">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
